e7329d4153c94c1046e8838fb6447314c87e9cea
Stèphanie Tabosa <stephanie.barreto@hotmail.com>
2022-08-19 11:38:13 -0300

    Criando AppBar Busca e Bottom NavigationBar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:coffee_app_ui/util/coffee_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text('Encontre o melhor café para você',
+                  style: GoogleFonts.bebasNeue(fontSize: 48)),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Encontre seu café...',
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600)),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                CoffeeTile(),
+                CoffeeTile(),
+                CoffeeTile(),
+              ],
+            ))
+          ],
+        ));
+  }
+}
